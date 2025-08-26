c908567bfb6d449cf1cd671547484d735b465e2b
Muneez Aslam <muneezmadyar@gmail.com>
2025-08-25 17:09:11 -0700

    updated port

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 // const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 // const _dirname = path.resolve();
 
