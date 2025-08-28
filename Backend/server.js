e114e9f421f53b6b05451acb22c51a9ebbc2e084
Muneez Aslam <muneezmadyar@gmail.com>
2025-08-27 17:21:01 -0700

    updated cors

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,15 @@ const PORT = process.env.PORT || 5000;
 // const _dirname = path.resolve();
 
 // ✅ Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",   // frontend
+    "http://localhost:5174",   // admin
+    "https://insu-licv.vercel.app", // deployed frontend
+    "https://your-admin.vercel.app"     // deployed admin
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
